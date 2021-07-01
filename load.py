4e94ab477ec77b1c5f0a3cbe4b353db6e9e94161
satyawiz <satyawiz@gmail.com>
2021-07-02 00:45:47 +0530

    'Digi-Mod'

--- load.py
+++ load.py
@@ -3,13 +3,13 @@ import os
 def init():
   cwd = os.getcwd()  # Get the current working directory (cwd)
   files = os.listdir(cwd)  # Get all the files in that directory
-  print("Files in %r: %s" % (cwd, files))
-  json_file = open('C:\\Users\\satye\\OneDrive\\Desktop\\Project\\Digit_Recog_Model\\model\\model.json','r')
+  # print("Files in %r: %s" % (cwd, files))
+  json_file = open(cwd + '\\model\\model.json','r')
   loaded_model_json = json_file.read()
   json_file.close()
   loaded_model = model_from_json(loaded_model_json)
   #load weights into new model
-  loaded_model.load_weights("C:\\Users\\satye\\OneDrive\\Desktop\\Project\\Digit_Recog_Model\\model\\mnist_model.h5")
+  loaded_model.load_weights(cwd + "\\model\\mnist_model.h5")
   print("Loaded Model from disk")
   #compile and evaluate loaded model
   loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
