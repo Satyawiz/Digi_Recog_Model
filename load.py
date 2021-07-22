763549769b9edd5f629a038d1b3f949b4c3f0fd4
satyawiz <satyawiz@gmail.com>
2021-07-22 10:37:02 +0530

    'Add'

--- load.py
+++ load.py
@@ -4,12 +4,12 @@ def init():
   cwd = os.getcwd()  # Get the current working directory (cwd)
   files = os.listdir(cwd)  # Get all the files in that directory
   # print("Files in %r: %s" % (cwd, files))
-  json_file = open(cwd + '\\model\\model.json','r')
+  json_file = open('model.json','r')
   loaded_model_json = json_file.read()
   json_file.close()
   loaded_model = model_from_json(loaded_model_json)
   #load weights into new model
-  loaded_model.load_weights(cwd + "\\model\\mnist_model.h5")
+  loaded_model.load_weights('mnist_model.h5')
   print("Loaded Model from disk")
   #compile and evaluate loaded model
   loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
